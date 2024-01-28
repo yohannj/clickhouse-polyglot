af8b813c1ce98dad1a3cbc1056961378bb48d98f
Yohann Jardin <yohann.jardin@telecomnancy.net>
2024-01-28 05:30:19 -0500

    feat(): print some function1

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from src.ch_types import *
 from src.client import Client
 
 if __name__ == '__main__':
@@ -5,12 +6,24 @@ if __name__ == '__main__':
 
     function_names = sorted([row[0] for row in client.execute("SELECT name, is_aggregate FROM system.functions")])
 
-    function0 = []
-    for function_name in function_names:
-        try:
-            client.execute(f"SELECT {function_name}()")
-            function0.append(function_name)
-        except:
-            pass
+    # function0 = []
+    # for function_name in function_names:
+    #     try:
+    #         client.execute(f"SELECT {function_name}()")
+    #         function0.append(function_name)
+    #     except:
+    #         pass
 
-    print(function0)
+    function1 = []
+    for function_name in function_names:
+        valid_type = []
+
+        for type1 in CHTypes:
+            arg1 = f"{type1.value['fuzzing_values'][0]}::{type1.value['name']}"
+            try:
+                client.execute(f"SELECT {function_name}({arg1})")
+                function1.append(function_name)
+            except:
+                pass
+
+    print(function1)
