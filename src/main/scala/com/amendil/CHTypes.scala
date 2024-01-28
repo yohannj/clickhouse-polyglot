05eaf47934334ccb231f3d57b849ba05413f8f5c
Yohann Jardin <yohann.jardin@telecomnancy.net>
2024-01-28 05:30:22 -0500

    chore(): Minor refacto CHType

--- src/main/scala/com/amendil/CHTypes.scala
+++ src/main/scala/com/amendil/CHTypes.scala
@@ -1,64 +1,64 @@
 package com.amendil
 
-enum CHTypes[T](val name: String, val fuzzingValues: Seq[T]) {
+enum CHType(val name: String, val fuzzingValues: Seq[String]) {
 
   // Numbers
-  case Int8 extends CHTypes[Short]("Int8", Seq(-128, 127))
-  case Int16 extends CHTypes[Short]("Int16", Seq(-32768, 32767))
-  case Int32 extends CHTypes[Int]("Int32", Seq(-2147483648, 2147483647))
-  case Int64 extends CHTypes[Long]("Int64", Seq(-9223372036854775808L, 9223372036854775807L))
+  case Int8 extends CHType("Int8", Seq("-128", "127"))
+  case Int16 extends CHType("Int16", Seq("-32768", "32767"))
+  case Int32 extends CHType("Int32", Seq("-2147483648", "2147483647"))
+  case Int64 extends CHType("Int64", Seq("-9223372036854775808", "9223372036854775807"))
   case Int128
-      extends CHTypes[BigInt](
+      extends CHType(
         "Int128",
-        Seq(BigInt("-170141183460469231731687303715884105728"), BigInt("170141183460469231731687303715884105727"))
+        Seq("-170141183460469231731687303715884105728", "170141183460469231731687303715884105727")
       )
   case Int256
-      extends CHTypes[BigInt](
+      extends CHType(
         "Int256",
         Seq(
-          BigInt("-57896044618658097711785492504343953926634992332820282019728792003956564819968"),
-          BigInt("57896044618658097711785492504343953926634992332820282019728792003956564819967")
+          "-57896044618658097711785492504343953926634992332820282019728792003956564819968",
+          "57896044618658097711785492504343953926634992332820282019728792003956564819967"
         )
       )
-  case UInt8 extends CHTypes[Short]("UInt8", Seq(0, 1, 255))
-  case UInt16 extends CHTypes[Int]("UInt16", Seq(0, 65535))
-  case UInt32 extends CHTypes[Long]("UInt32", Seq(0, 4294967295L))
-  case UInt64 extends CHTypes[BigInt]("UInt64", Seq(0, BigInt("18446744073709551615")))
-  case UInt128 extends CHTypes[BigInt]("UInt128", Seq(0, BigInt("340282366920938463463374607431768211455")))
+  case UInt8 extends CHType("UInt8", Seq("0", "1", "255"))
+  case UInt16 extends CHType("UInt16", Seq("0", "65535"))
+  case UInt32 extends CHType("UInt32", Seq("0", "4294967295"))
+  case UInt64 extends CHType("UInt64", Seq("0", "18446744073709551615"))
+  case UInt128 extends CHType("UInt128", Seq("0", "340282366920938463463374607431768211455"))
   case UInt256
-      extends CHTypes[BigInt](
+      extends CHType(
         "UInt256",
-        Seq(0, BigInt("115792089237316195423570985008687907853269984665640564039457584007913129639935"))
+        Seq("0", "115792089237316195423570985008687907853269984665640564039457584007913129639935")
       )
 
-  case Float32 extends CHTypes[String]("Float32", Seq("-inf", "nan", "0.5"))
-  case Float64 extends CHTypes[String]("Float64", Seq("-inf", "nan", "0.5"))
+  case Float32 extends CHType("Float32", Seq("-inf", "nan", "0.5"))
+  case Float64 extends CHType("Float64", Seq("-inf", "nan", "0.5"))
 
   // Others
-  case StringType extends CHTypes[String]("String", Seq("'foo'"))
+  case StringType extends CHType("String", Seq("'foo'"))
 }
 
-enum CHAbstractTypes(val fuzzingValue: Any, val chTypes: Seq[CHTypes[_]]) {
+enum CHAbstractTypes(val fuzzingValue: Any, val chTypes: Seq[CHType]) {
   case Numbers
       extends CHAbstractTypes(
         1,
         Seq(
-          CHTypes.Int8,
-          CHTypes.Int16,
-          CHTypes.Int32,
-          CHTypes.Int64,
-          CHTypes.Int128,
-          CHTypes.Int256,
-          CHTypes.UInt8,
-          CHTypes.UInt16,
-          CHTypes.UInt32,
-          CHTypes.UInt64,
-          CHTypes.UInt128,
-          CHTypes.UInt256,
-          CHTypes.Float32,
-          CHTypes.Float64
+          CHType.Int8,
+          CHType.Int16,
+          CHType.Int32,
+          CHType.Int64,
+          CHType.Int128,
+          CHType.Int256,
+          CHType.UInt8,
+          CHType.UInt16,
+          CHType.UInt32,
+          CHType.UInt64,
+          CHType.UInt128,
+          CHType.UInt256,
+          CHType.Float32,
+          CHType.Float64
         )
       )
 
-  case String extends CHAbstractTypes(CHTypes.StringType.fuzzingValues.head, Seq(CHTypes.StringType))
+  case String extends CHAbstractTypes(CHType.StringType.fuzzingValues.head, Seq(CHType.StringType))
 }
--- src/main/scala/com/amendil/bo/CHFunctionFuzzResult.scala
+++ src/main/scala/com/amendil/bo/CHFunctionFuzzResult.scala
@@ -1,14 +1,14 @@
 package com.amendil.bo
 
-import com.amendil.CHTypes
+import com.amendil.CHType
 
 final case class CHFunctionFuzzResult(
     name: String,
-    functionNTypes: Seq[CHTypes[_]] = Nil,
+    functionNTypes: Seq[CHType] = Nil,
     isFunction0: Boolean = false,
-    function1Types: Seq[CHTypes[_]] = Nil,
-    function2Types: Seq[(CHTypes[_], CHTypes[_])] = Nil,
-    function3Types: Seq[(CHTypes[_], CHTypes[_], CHTypes[_])] = Nil
+    function1Types: Seq[CHType] = Nil,
+    function2Types: Seq[(CHType, CHType)] = Nil,
+    function3Types: Seq[(CHType, CHType, CHType)] = Nil
 ):
   override def toString(): String =
     s"$name(isFunctionN=${functionNTypes.nonEmpty}, isFunction0=$isFunction0, isFunction1=${function1Types.nonEmpty}, isFunction2=${function2Types.nonEmpty}, isFunction3=${function3Types.nonEmpty})"
